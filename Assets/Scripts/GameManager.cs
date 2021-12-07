455fbc2c698e1d9cb5f9fe1806eba3ac661cea59
Forzeny <95419340+Forzeny@users.noreply.github.com>
2021-12-07 20:47:22 +0800

    add join game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,60 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    public static GameManager instance;
+    string gameVersion = "1";
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogErrorFormat(gameObject,
+                    $"Multiple instances of {GetType().Name} is not allow");
+            DestroyImmediate(gameObject);
+            return;
+        }
+        PhotonNetwork.AutomaticallySyncScene = true;
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+    }
     void Start()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = gameVersion;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnected()
     {
-        
+        Debug.Log("PUN Connected");
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Connected to Master");
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat($"PUN Disconnected was called by PUN with reason {cause}");
+    }
+
+    public void JoinGameRoom()
+    {
+        var options = new RoomOptions
+        {
+            MaxPlayers = 6
+        };
+        PhotonNetwork.JoinOrCreateRoom("BattleField", options, null);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room!!");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError($"Joined room failed!! Message : {message}");
     }
 }
